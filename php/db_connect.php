c13013205175b5434a38ff954e3eb9c05e1eb6c4
simonjaeggi <simon.jaeggi@gibmit.ch>
2019-06-05 20:48:54 +0200

    Sql

--- php/db_connect.php
+++ php/db_connect.php
@@ -1,14 +1,14 @@
 <?php
 //Benötigte Informationen
 $host = 'localhost';
-$username = 'root';
-$password = '';
-$database = 'user';
+$username = 'kochbuch_editor';
+$password = 'aDSF123@#§1sdf';
+$database = 'kochbuch';
 
 //Instanzierung der Verbindungsvariable
 $mysqli = new mysqli($host, $username, $password, $database);
 
-//Fehlermeldung falls Verbindung fehlschlägt. 
+//Fehlermeldung falls Verbindung fehlschlägt.
 if ($mysqli->connect_error) {
   die('Connect Error (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
 }
