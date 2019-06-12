962084c271f5d92da55c5797e976a7f7ef1aac07
Your Name <you@example.com>
2019-06-12 15:15:15 +0200

    sql

--- sql/sql_Kochbuch.sql
+++ sql/sql_Kochbuch.sql
@@ -5,138 +5,104 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
 -- -----------------------------------------------------
--- Database Kochbuch
+-- Schema mydb
 -- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `mydb` ;
 
 -- -----------------------------------------------------
--- Database Kochbuch
+-- Schema mydb
 -- -----------------------------------------------------
-DROP database if exists Kochbuch;
-CREATE Database `Kochbuch` DEFAULT CHARACTER SET utf8 ;
-USE `Kochbuch` ;
+CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
+USE `mydb` ;
 
 -- -----------------------------------------------------
--- Table `Kochbuch`.`tbl_Rezept`
+-- Table `mydb`.`tbl_Benutzer`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `Kochbuch`.`tbl_Rezept` ;
+DROP TABLE IF EXISTS `mydb`.`tbl_Benutzer` ;
 
-CREATE TABLE IF NOT EXISTS `Kochbuch`.`tbl_Rezept` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `Autor` VARCHAR(45) NULL,
-  `Gericht` VARCHAR(100) NOT NULL,
-  PRIMARY KEY (`id`))
+CREATE TABLE IF NOT EXISTS `mydb`.`tbl_Benutzer` (
+  `ID` INT NOT NULL AUTO_INCREMENT,
+  `Email` VARCHAR(50) NOT NULL,
+  `HashedPassword` VARCHAR(255) NOT NULL,
+  `Firstname` VARCHAR(50) NOT NULL,
+  `Lastname` VARCHAR(50) NOT NULL,
+  `Username` VARCHAR(50) NOT NULL,
+  PRIMARY KEY (`ID`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `Kochbuch`.`tbl_Zutat`
+-- Table `mydb`.`tbl_Rezept`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `Kochbuch`.`tbl_Zutat` ;
+DROP TABLE IF EXISTS `mydb`.`tbl_Rezept` ;
 
-CREATE TABLE IF NOT EXISTS `Kochbuch`.`tbl_Zutat` (
-  `id` INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `mydb`.`tbl_Rezept` (
+  `ID` INT NOT NULL AUTO_INCREMENT,
+  `Benutzer_ID` INT NULL,
+  `Name` VARCHAR(50) NOT NULL,
+  `Tipp` MEDIUMTEXT NULL,
+  `ExtAutor` VARCHAR(45) NULL,
+  PRIMARY KEY (`ID`),
+  INDEX `fk_Rezept_Benutzer1_idx` (`Benutzer_ID` ASC),
+  CONSTRAINT `fk_Rezept_Benutzer1`
+    FOREIGN KEY (`Benutzer_ID`)
+    REFERENCES `mydb`.`tbl_Benutzer` (`ID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`tbl_Zutat`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `mydb`.`tbl_Zutat` ;
+
+CREATE TABLE IF NOT EXISTS `mydb`.`tbl_Zutat` (
+  `ID` INT NOT NULL AUTO_INCREMENT,
   `Name` VARCHAR(45) NOT NULL,
-  `Typ` VARCHAR(45) NULL,
-  PRIMARY KEY (`id`))
+  PRIMARY KEY (`ID`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `Kochbuch`.`tbl_Zutaten_Rezept`
+-- Table `mydb`.`tbl_Zubereitung`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `Kochbuch`.`tbl_Zutaten_Rezept` ;
+DROP TABLE IF EXISTS `mydb`.`tbl_Zubereitung` ;
 
-CREATE TABLE IF NOT EXISTS `Kochbuch`.`tbl_Zutaten_Rezept` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `tbl_Rezept_id` INT NOT NULL,
-  `tbl_Zutat_id` INT NOT NULL,
-  `Menge` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_tbl_Zutaten_Rezept_tbl_Rezept_idx` (`tbl_Rezept_id` ASC),
-  INDEX `fk_tbl_Zutaten_Rezept_tbl_Zutat1_idx` (`tbl_Zutat_id` ASC),
-  CONSTRAINT `fk_tbl_Zutaten_Rezept_tbl_Rezept`
-    FOREIGN KEY (`tbl_Rezept_id`)
-    REFERENCES `Kochbuch`.`tbl_Rezept` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_tbl_Zutaten_Rezept_tbl_Zutat1`
-    FOREIGN KEY (`tbl_Zutat_id`)
-    REFERENCES `Kochbuch`.`tbl_Zutat` (`id`)
+CREATE TABLE IF NOT EXISTS `mydb`.`tbl_Zubereitung` (
+  `ID` INT NOT NULL AUTO_INCREMENT,
+  `Rezept_ID` INT NOT NULL,
+  `Zubereitungsschritt` MEDIUMTEXT NULL,
+  PRIMARY KEY (`ID`, `Rezept_ID`),
+  INDEX `fk_Zubereitung_Rezept1_idx` (`Rezept_ID` ASC),
+  CONSTRAINT `fk_Zubereitung_Rezept1`
+    FOREIGN KEY (`Rezept_ID`)
+    REFERENCES `mydb`.`tbl_Rezept` (`ID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `Kochbuch`.`tbl_Arbeitsschritt`
+-- Table `mydb`.`tbl_Zutatenmenge`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `Kochbuch`.`tbl_Arbeitsschritt` ;
+DROP TABLE IF EXISTS `mydb`.`tbl_Zutatenmenge` ;
 
-CREATE TABLE IF NOT EXISTS `Kochbuch`.`tbl_Arbeitsschritt` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `Text` TEXT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `Kochbuch`.`tbl_Arbeitsschritte_Rezept`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `Kochbuch`.`tbl_Arbeitsschritte_Rezept` ;
-
-CREATE TABLE IF NOT EXISTS `Kochbuch`.`tbl_Arbeitsschritte_Rezept` (
-  `tbl_Rezept_id` INT NOT NULL,
-  `tbl_Arbeitsschritt_id` INT NOT NULL,
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `Arbeitsschritt_Nummer` VARCHAR(45) NOT NULL,
-  INDEX `fk_tbl_Arbeitsschritte_Rezept_tbl_Rezept1_idx` (`tbl_Rezept_id` ASC),
-  INDEX `fk_tbl_Arbeitsschritte_Rezept_tbl_Arbeitsschritt1_idx` (`tbl_Arbeitsschritt_id` ASC),
-  PRIMARY KEY (`id`),
-  CONSTRAINT `fk_tbl_Arbeitsschritte_Rezept_tbl_Rezept1`
-    FOREIGN KEY (`tbl_Rezept_id`)
-    REFERENCES `Kochbuch`.`tbl_Rezept` (`id`)
+CREATE TABLE IF NOT EXISTS `mydb`.`tbl_Zutatenmenge` (
+  `Rezept_ID` INT NOT NULL,
+  `Zutat_ID` INT NOT NULL,
+  `Menge` VARCHAR(20) NOT NULL,
+  PRIMARY KEY (`Rezept_ID`, `Zutat_ID`),
+  INDEX `fk_Rezept_has_Zutat_Zutat1_idx` (`Zutat_ID` ASC),
+  INDEX `fk_Rezept_has_Zutat_Rezept1_idx` (`Rezept_ID` ASC),
+  CONSTRAINT `fk_Rezept_has_Zutat_Rezept1`
+    FOREIGN KEY (`Rezept_ID`)
+    REFERENCES `mydb`.`tbl_Rezept` (`ID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_tbl_Arbeitsschritte_Rezept_tbl_Arbeitsschritt1`
-    FOREIGN KEY (`tbl_Arbeitsschritt_id`)
-    REFERENCES `Kochbuch`.`tbl_Arbeitsschritt` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `Kochbuch`.`tbl_Tipp`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `Kochbuch`.`tbl_Tipp` ;
-
-CREATE TABLE IF NOT EXISTS `Kochbuch`.`tbl_Tipp` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `Text` TEXT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `Kochbuch`.`tbl_Tipps_Rezept`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `Kochbuch`.`tbl_Tipps_Rezept` ;
-
-CREATE TABLE IF NOT EXISTS `Kochbuch`.`tbl_Tipps_Rezept` (
-  `tbl_Rezept_id` INT NOT NULL,
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `tbl_Tipp_id` INT NOT NULL,
-  INDEX `fk_tbl_Tipps_Rezept_tbl_Rezept1_idx` (`tbl_Rezept_id` ASC),
-  PRIMARY KEY (`id`),
-  INDEX `fk_tbl_Tipps_Rezept_tbl_Tipp1_idx` (`tbl_Tipp_id` ASC),
-  CONSTRAINT `fk_tbl_Tipps_Rezept_tbl_Rezept1`
-    FOREIGN KEY (`tbl_Rezept_id`)
-    REFERENCES `Kochbuch`.`tbl_Rezept` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_tbl_Tipps_Rezept_tbl_Tipp1`
-    FOREIGN KEY (`tbl_Tipp_id`)
-    REFERENCES `Kochbuch`.`tbl_Tipp` (`id`)
+  CONSTRAINT `fk_Rezept_has_Zutat_Zutat1`
+    FOREIGN KEY (`Zutat_ID`)
+    REFERENCES `mydb`.`tbl_Zutat` (`ID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
