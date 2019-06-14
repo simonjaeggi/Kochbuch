e4393d92839146989d30f93ca07542db961c87e8
Lukas Reinhardt <lukas.reinhardt@idorsia.com>
2019-06-14 14:13:06 +0200

    mini fix an sql test user ihbindig

--- sql/kochbuch_editor.sql
+++ sql/kochbuch_editor.sql
@@ -1,4 +1,3 @@
-CREATE USER 'kochbuch_editor'@'localhost' identified by 'ASdf123:*SDsd';
+CREATE USER 'kochbuch_editor'@'localhost' IDENTIFIED WITH mysql_native_password BY 'ASdf123:*SDsd';
 grant select, insert, update, delete on kochbuch.* to 'kochbuch_editor'@'localhost';
 FLUSH PRIVILEGES;
-
