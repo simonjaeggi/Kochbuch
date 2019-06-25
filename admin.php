55fed72dd0bd93867059f1ad275ec7448350a25c
simonjaeggi <simon.jaeggi@gibmit.ch>
2019-06-25 12:37:32 +0200

    angefangen

--- admin.php
+++ admin.php
@@ -17,6 +17,10 @@ if (!isset($_SESSION['loggedin'])) {
  
 <?php include("includes/filestart.php"); ?>
 <!-- Fill content from here -->
+<h1 class="title has-text-white">Konto</h1>
+Eingeloggt als:  <?php echo $_SESSION['username'] ?>
+<h2 class="subtitle has-text-white">Passwort zurücksetzen</h2>
+
 
 <!-- Fill content to here -->
 <?php include("includes/fileend.php"); ?>
